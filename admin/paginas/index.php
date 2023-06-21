04aa468417ef43f370402c6de14de2e3e7128dd8
santiagoma7 <129330790+santiagoma7@users.noreply.github.com>
2023-06-21 17:37:50 -0500

    Update index.php

---
+++ admin/paginas/index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type ="text/css" href="../../estilos/nav.css">
+  </head>
+ <body>
+	<nav class="navbar">
+		<div class="brand-title"><p>Administrador</p></div>
+		<a href="#" class="toggle-button">
+			<span class="bar"></span><!--hamburguesa-->
+			<span class="bar"></span>
+			<span class="bar"></span>
+		</a>
+		<div class="navbar-links">
+			<ul>
+				<li><a class="doc" href="../../docentes/paginas/agregar.php">Docentes</a></li>
+				<li><a class="est" href="../../estudiantes/paginas/agregar.php">Estudiantes</a></li>
+				<li><a class="mat" href="../../materias/agregar.php">Materias</a></li>
+                <button class="boton" >Cerrar sesión</button>
+			</ul>
+		</div>
+</body>
+</html>
+
