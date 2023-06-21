176df1660ba436d1c39208b765acf3d8135fc450
santiagoma7 <129330790+santiagoma7@users.noreply.github.com>
2023-06-21 17:18:05 -0500

    Update agregar.php

--- admin/paginas/agregar.php
+++ admin/paginas/agregar.php
@@ -3,67 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type ="text/css" href="estilos/estilos.css">
+    <link rel="stylesheet" type ="text/css" href="../../estilos/registroform.css">
     <title>Registro</title>
 </head>
 <body>
-    <style>
-        body {
-            background-color: #f8f9fa;
-        }
-
-        .container {
-            max-width: 500px;
-            margin: 0 auto;
-            padding-top: 100px;
-            text-align: center;
-        }
-
-        form {
-            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-            background-color: purple;
-            padding: 30px;
-            border-radius: 20px;
-            color: white;
-            margin: 0 auto;
-            width: 300px;
-        }
-
-        form label {
-            display: block;
-            margin-bottom: 3px;
-        }
-
-        form input[type="text"],
-        form input[type="password"] {
-            width: 100%;
-            padding: 6px;
-            margin-bottom: 10px;
-        }
-
-        form button {
-            width: 100%;
-            padding: 12px;
-            background-color: #6c757d;
-            border: none;
-            color: white;
-            cursor: pointer;
-        }
-
-        form button:hover {
-            background-color: #495057;
-        }
-
-        form p {
-            text-align: center;
-            margin-top: 10px;
-        }
-
-        form a {
-            color: white;
-            text-decoration: underline;
-        }
-    </style>
+   
 
     <div class="container">
         <form>
