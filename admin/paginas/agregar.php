37077841ee62f0d918db3578180b0b6ff284797e
santiagoma7 <129330790+santiagoma7@users.noreply.github.com>
2023-06-22 15:22:21 -0500

    Update agregar.php

--- admin/paginas/agregar.php
+++ admin/paginas/agregar.php
@@ -1,27 +1,52 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type ="text/css" href="../../estilos/nav.css">
-  </head>
- <body>
-	<nav class="navbar">
-		<div class="brand-title"><p>Administrador</p></div>
-		<a href="#" class="toggle-button">
-			<span class="bar"></span><!--hamburguesa-->
-			<span class="bar"></span>
-			<span class="bar"></span>
-		</a>
-		<div class="navbar-links">
-			<ul>
-				<li><a class="doc" href="../../docentes/paginas/agregar.php">Docentes</a></li>
-				<li><a class="est" href="../../estudiantes/paginas/agregar.php">Estudiantes</a></li>
-				<li><a class="mat" href="../../materias/agregar.php">Materias</a></li>
-                <button class="boton" >Cerrar sesión</button>
-			</ul>
-		</div>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type ="text/css" href="../../estilos/registroform.css">
+    <title>Registro</title>
+</head>
+<body>
+   
+
+    <div class="container">
+        <form action="../controladores/agregarusuario.php" method="POST">
+            <label>Nombre</label>
+            <br>
+            <input type="text" placeholder="Nombre" name="txtnombre">
+            <br>
+            <label>Apellido</label>
+            <br>
+            <input type="text" name="txtapellido" placeholder="Apellido">
+            <br>
+            <label>Usuario</label>
+            <br>
+            <input type="text" name="txtusuario" placeholder="Usuario">
+            <br>
+            <label>Contraseña</label>
+            <br>
+            <input name="txtcontrasena" type="password" placeholder="Contraseña">
+            <br>
+            <p>Perfil
+            <label for="perfil"></label>
+            <select name="txtperfil">
+                <option></option>
+                <option value="Administrador">Administrador</option>
+                <option value="Docente">Docentes</option>
+                
+            </select>
+</p>
+<p>Estado
+            <label for="perfil"></label>
+            <select name="txtestado">
+                <option></option>
+                <option value="Activo">Activo</option>
+                <option value="No activo">No activo</option>
+                
+            </select>
+</p>
+            <input type="submit">
+        </form>
+    </div>
 </body>
 </html>
-
